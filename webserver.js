c8a4bd6ea7ceb6379923762bf8b44db28613b8e2
Joseph Radford <jsphradford@gmail.com>
2020-05-16 10:24:44 -0500

    Added server support for socket.io

--- webserver.js
+++ webserver.js
@@ -1,10 +1,11 @@
 var http = require('http').createServer(handler); // require http server, and create server with function handler()
 var fs = require('fs'); // require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 
 http.listen(8080); // listen on port 8080
 
 function handler (req, res) { //  create server
-	fs.readFile(_dirname + '/public/index.html', function(err, data) { // read file index.html in public folder
+	fs.readFile(__dirname + '/public/index.html', function(err, data) { // read file index.html in public folder
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'}); // display 404 on error
 			return res.end("404 Not Found");
@@ -14,3 +15,13 @@ function handler (req, res) { //  create server
 		return res.end();
 	});
 }
+
+io.sockets.on('connection', function(socket) {//WebSocket Connection
+	var lightvalue = 0; //static variable for current status
+	socket.on('light', function(data) { //Get light switch status from client
+		lightvalue = data;
+		if (lightvalue) {
+			console.log(lightValue); //turn LED on or off, for now just log in console
+		}
+	});
+});
